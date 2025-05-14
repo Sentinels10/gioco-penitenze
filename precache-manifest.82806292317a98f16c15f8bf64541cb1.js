8ec73a62b1283575d26178e7010d976f30b47601
Sentinels10 <10sentinels01@gmail.com>
2025-05-14 16:18:51 +0200

    Updates

---
+++ precache-manifest.82806292317a98f16c15f8bf64541cb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60eb17a664d8559b9c648ce07c07c541",
+    "url": "/gioco-penitenze/index.html"
+  },
+  {
+    "revision": "cc6a2fad74584170ab12",
+    "url": "/gioco-penitenze/static/css/main.1aa36282.chunk.css"
+  },
+  {
+    "revision": "2f375dc850f0dc1ef57a",
+    "url": "/gioco-penitenze/static/js/0.d1f6d920.chunk.js"
+  },
+  {
+    "revision": "ffe79ec3ceaf20a60b02",
+    "url": "/gioco-penitenze/static/js/1.9b86c751.chunk.js"
+  },
+  {
+    "revision": "7b9e6a794e5d909eaa60",
+    "url": "/gioco-penitenze/static/js/2.c4b327f9.chunk.js"
+  },
+  {
+    "revision": "5680800299cb7324cea6",
+    "url": "/gioco-penitenze/static/js/5.5a018b3d.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/gioco-penitenze/static/js/5.5a018b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6a2fad74584170ab12",
+    "url": "/gioco-penitenze/static/js/main.8177667b.chunk.js"
+  },
+  {
+    "revision": "b9e3d3f426049d466888",
+    "url": "/gioco-penitenze/static/js/runtime-main.9be77a88.js"
+  },
+  {
+    "revision": "f00b599bced6dea980776d79e63baede",
+    "url": "/gioco-penitenze/static/media/pointing-glove.f00b599b.png"
+  }
+]);
